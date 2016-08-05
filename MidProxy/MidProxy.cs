5720e32acfcfcce9b773c3a4574fd9ccd031a2cf
Bjorn van Dommelen <bjorn.van.dommelen@gmail.com>
2016-08-05 23:05:14 +0200

    Added Service project

---
+++ MidProxy/MidProxy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MidProxy
+{
+    public partial class MidProxy : ServiceBase
+    {
+        private bool isStopRequested = false;
+
+        public MidProxy()
+        {
+            InitializeComponent();
+        }
+
+        public void DbgStart(string[] args)
+        {
+            Debug.Listeners.Add(new ConsoleTraceListener());
+            Debug.WriteLine("Debug: started service MidProxy as command line application");
+            var t = new Thread(new ThreadStart(() => this.OnStart(args)));
+            t.Start();
+            Console.Read();
+            OnStop();
+            t.Join();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            while (!isStopRequested)
+            {
+                Debug.WriteLine("Args: " + string.Join(",", args));
+                Thread.Sleep(5000);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            isStopRequested = true;
+        }
+    }
+}
---
+++ MidProxy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidProxy
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if (args.Contains("-debug"))
+            {
+                (new MidProxy()).DbgStart(args);
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new MidProxy()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
